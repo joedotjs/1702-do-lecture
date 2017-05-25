163bf1ba06d8e70f293ffc01fe91867de4c5d60c
Joe Alves <joe@fullstackacademy.com>
2017-05-25 17:32:12 -0400

    my app is finished

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.listen(3001, () => {
+    console.log('App on!')
+});
+
+app.get('/', (_, res) => {
+    res.send('Hello! I am a deployed app!');
+});
